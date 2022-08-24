e5b8299a26de019050f7d264370666ce2a1ab21d
Ilya <ikotovnic@gmail.com>
2022-08-24 19:50:39 +0300

    24-08-22 330

--- js/ikotov-tf.js
+++ js/ikotov-tf.js
@@ -168,7 +168,7 @@ async function trainModel(model, inputs, labels) {
   });
   
   const batchSize = 2000000;
-  const epochs = 300;
+  const epochs = 330;
   
   return await model.fit(inputs, labels, {
     batchSize,
